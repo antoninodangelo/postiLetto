9e9442980c31e8c9e681b5bf676cce232e6fb0a1
antoninodangelo <antoninodangelo@users.noreply.github.com>
2026-08-26 20:58:11 +0200

    inserito ssl per mysql

--- config/db.js
+++ config/db.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 
 dotenv.config(); // Carica le variabili dal .env
 
+const useSSL = process.env.NODE_ENV ==="product";
+console.log(process.env.NODE_ENV);
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -14,9 +16,9 @@ export const pool = mysql.createPool({
   connectionLimit: 3,
   queueLimit: 0,
   multipleStatements: true,
-   ssl: {
-    rejectUnauthorized: false // Consente l'uso del certificato self-signed di Aiven
-  }
+  /*  ssl: {
+     ssl: useSSL ? { rejectUnauthorized: false } : false
+  } */
 });
 
 // Test connessione (opzionale ma utile)
